e7d98452b45229e4a858a5d5d9789150ee235364
lili <lili@>
2023-03-22 09:32:47 +0100

    elso JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+//console.log("Hello world!")
+//let vezetekNev = "0"
+//let keresztNev = "Gábor"
+//let szamlalo= 0
+//console.log(vezetekNev + "" +keresztNev, szamlalo)
+
+//if (vezetekNev == szamlalo) {
+  //  console.log("a vezetekNev egyenlő a szamlalo-val")
+//} else {
+  //  console.log("a vezetekNev NEM egyenlő a szamlalo-val")
+//}
+
+function login() {
+
+    console.log("KATT")
+    let passwordField = querySelector("#pass")
+   // console.log(passwordField.value)
+
+    if (passwordField.value== "nagytitiok") 
+     } 
+     else{
+         
+     }
